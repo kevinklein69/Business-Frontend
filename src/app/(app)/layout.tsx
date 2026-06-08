f248d632ae1740553de2cf9e3fd99851ab767267
kevinklein <klein.k69@web.de>
2026-06-08 15:03:12 +0200

    fixed width when scrollbar appears

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -26,7 +26,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <Sidebar />
       <div className="flex flex-col flex-1 overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-y-auto p-6">{children}</main>
+        <main className="flex-1 overflow-y-auto p-6 [scrollbar-gutter:stable]">{children}</main>
       </div>
     </div>
   )
